0326b6a3ff4923b7d1989c2b148062e44b6c9b7f
sisilass31 <lassarsihem31@gmail.com>
2023-05-17 16:29:26 +0200

    third commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ db.connect((error) => {
 
 app.use(express.json())
 
-// fonction qui génère le token:
+// fonction qui génère/créer le token:
 function generate_token(user) {
     const payload = {
         id: user.id,
@@ -59,7 +59,6 @@ function verify_token(req, res, next){
 
 /* fonction permettant le créer un utilisateur:
 --> créer une nouvelle page*/
-
 app.post('/user', (req, res) => {
     const {name, password, role, email} = req.body
     bcrypt.hash(password, 10, (error, hashedPassword) => {
@@ -68,7 +67,7 @@ app.post('/user', (req, res) => {
             res.status(500).json({ error: 'operation failed' })
         } else {
             const new_user = { name, password: hashedPassword, role, email }
-            db.query('insert into user set?', new_user, (error, result) => {
+            db.query('INSERT INTO users SET ?', new_user, (error, result) => {
                 if(error) {
                     console.log(error)
                     res.status(500).json({ error: 'operation failed' })
@@ -84,9 +83,104 @@ app.post('/user', (req, res) => {
         }
     })
 })
+
+// login 
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    db.query('SELECT * FROM users WHERE email = ?', email, (error, result) => {
+        if(error){
+            console.log(error);
+            res.status(500).json({error: 'failed to login'})
+        } else if(result.length === 0){
+            res.status(401).json({ error: 'invalid email or password' })
+        } else {
+            const user = result[0]
+            bcrypt.compare(password, user.password, (error, isMatch) => {
+                if(error){
+                    console.log(error)
+                    res.status(500).json({ error: 'failed to login' })
+                } else if(isMatch){
+                    const token = generate_token(user)
+                    res.status(200).json({
+                        message: 'login sucessfull',
+                        token: token,
+                    })
+                } else {
+                    res.status(401).json({ error: 'invalid email or password' })
+                }
+            })
+        }
+    }) 
+})
+
+// récupérer tous les users
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (error, results) => {
+        if(error){
+            console.log(error);
+            res.status(500).json({ error: 'failed to receive users'});
+        } else {
+            res.status(200).json(results)
+        }
+    })
+})
+
+// récupérer un seul user
+app.get('/user/:id', (req, res) => {
+    const user_id = req.params.id
+    console.log(user_id);
+    db.query('SELECT * FROM users WHERE id = ?', user_id, (error, results) => {
+        if(error){
+            console.log(error);
+            res.status(500).json({ error: 'failed to receive the user'})
+        } else if (results.length === 0){
+            res.status(404).json({ error: 'user not found'});
+        } else {
+            res.status(200).json(results[0]);
+        }
+    })
+})
+
+// update 
+app.put('/user/:id', (req, res) => {
+    const user_id = req.params.id
+    const { name, password, role, email} = req.body;
+    const update_user = { name, password, role, email }
+    db.query(
+        'UPDATE users SET ? WHERE id = ?',
+        [update_user, user_id],
+        (error, result) => {
+            if(error){
+                console.log(error);
+                res.status(500).json({ error: 'failed to update the user'})
+            } else if (result.affedctedRows === 0) {
+                res.status(404).json({ error: 'user not found'});
+            } else {
+                res.status(200).json({ message: 'user updated successfully'})
+            }
+        }
+    )
+})
+
+// delete 
+app.delete('/user/:id', (req, res) => {
+    const user_id = req.params.id
+    db.query(
+        'DELETE FROM users WHERE id = ?', user_id, (error, result) => {
+            if(error){
+                console.log(error);
+                res.status(500).json({ error: 'failed to delete the user'})
+            } else if (result.affedctedRows === 0) {
+                res.status(404).json({ error: 'user not found'});
+            } else {
+                res.status(200).json({ message: 'user deleted successfully'})
+            }
+        }
+    )
+})
+
 // notre serveur sera sur le port 3306:
 const port =  process.env.PORT || 3306;
-
 app.listen(port, () => {
     console.log(`Notre application est démarrrée sur : http://localhost:${port}`)
 })
